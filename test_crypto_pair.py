5ba68a4accc14fa3c23adee806127db885d55687
soulmachine <soulmachine@gmail.com>
2021-12-06 13:38:45 +0000

    Updated crypto-pair-ffi

--- test_crypto_pair.py
+++ test_crypto_pair.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from crypto_pair import normalize_pair
+from crypto_pair import MarketType, get_market_type, normalize_pair
 
 
 def test_binance():
     assert normalize_pair('BTCUSDT', 'binance') == 'BTC/USDT'
+    assert get_market_type('BTCUSD_PERP', 'binance') == MarketType.inverse_swap
